5dcdf5f65ce098e24e2c94617a9d4682cd40d48b
Umer Nasir <umer.nas@outlook.com>
2021-09-20 12:26:31 +0500

    Added MongoDB & Mongoose | Video 6

--- API/routes/products.js
+++ API/routes/products.js
@@ -1,39 +1,87 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Product = require("../models/Product");
 
 router.get("/", (req, res) => {
-  res.status(200).json({
-    message: "GET route for products",
-  });
+  Product.find()
+    .exec()
+    .then((data) => {
+      console.log(data);
+      res.status(200).json(data);
+    })
+    .catch((err) => console.log(err));
+  // res.status(200).json({
+  //   message: "GET route for products",
+  // });
 });
 
 router.get("/:productId", (req, res) => {
   const id = req.params.productId;
-  if (id === "special") {
-    return res.status(200).json({
-      message: "GET route for SPECIAL products",
-    });
-  }
-  res.status(200).json({
-    message: `GET route for products ${id}`,
-  });
+  Product.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log(doc);
+      res.status(200).json(doc);
+    })
+    .catch((err) => console.log(err));
 });
 
 router.patch("/:productId", (req, res) => {
-  res.status(200).json({
-    message: `UPDATED products route`,
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (let ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+
+  Product.findByIdAndUpdate(id, { $set: updateOps })
+    .exec()
+    .then((updatedDoc) => {
+      console.log(updatedDoc);
+      res.status(200).json(updatedDoc);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        err,
+      });
+    });
 });
 
 router.delete("/:productId", (req, res) => {
-  res.status(200).json({
-    message: `DELETE products route`,
-  });
+  const id = req.params.productId;
+  Product.findByIdAndRemove(id)
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error,
+      });
+    });
 });
 
 router.post("/", (req, res) => {
+  const { name, price } = req.body;
+  const product = new Product({
+    _id: mongoose.Types.ObjectId(),
+    name: name,
+    price: price,
+  });
+
+  console.log(product);
+
+  product
+    .save(product)
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
+
   res.status(201).json({
     message: "POST route for products",
+    createdProduct: product,
   });
 });
 
--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const productRoutes = require("./API/routes/products");
 const orderRoutes = require("./API/routes/orders");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  `mongodb+srv://Umer:DBPassword123@umercluster.038er.mongodb.net/SHOP_DB?retryWrites=true&w=majority`
+);
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.header("Allow-Access-Control-Origin", "*");
+  res.header(
+    "Allow-Access-Control-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Allow-Access-Control-Methods", "DELETE, PATCH, PUT, POST, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: "It Works",
+  const error = new Error("Not Found.");
+  error.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
   });
 });
 
