63d718970149473015b1bff632f690173386c0b5
UmerNasir-2000 <umer.nas@outlook.com>
2021-12-27 20:20:45 +0500

    Test

--- API/models/Product.js
+++ API/models/Product.js
@@ -4,6 +4,7 @@ const productSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: String,
   price: Number,
+  quantity: Number,
 });
 
 module.exports = mongoose.model("Product", productSchema);
